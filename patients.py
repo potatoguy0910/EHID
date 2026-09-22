798d235b1ba1927611c13a6d426289c636eb0994
Vivek Mishra <email091010@gmail.com>
2026-09-22 21:13:52 +0530

    Create patients.py

---
+++ patients.py
@@ -0,0 +1,23 @@
+patients = {
+    1: {
+        "name": "Aarav Patel",
+        "age": 24,
+        "blood_group": "B+",
+        "allergies": "Penicillin",
+        "conditions": "Asthma",
+        "surgeries": "Appendectomy - 2024",
+        "medications": "Salbutamol",
+        "emergency_contact": "+91 XXXXX XXXXX"
+    },
+
+    2: {
+        "name": "Rohan Shah",
+        "age": 31,
+        "blood_group": "O+",
+        "allergies": "None",
+        "conditions": "Diabetes",
+        "surgeries": "None",
+        "medications": "Metformin",
+        "emergency_contact": "+91 XXXXX XXXXX"
+    }
+}
